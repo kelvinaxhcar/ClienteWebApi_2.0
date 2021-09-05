4ac3c5094f8ab86a09d8b08dfe906bb1c3b7f2d8
Kelvin Mateus Axhcar de Jesus <56117421+kelvinaxhcar@users.noreply.github.com>
2021-09-05 13:59:38 -0300

    Add files via upload
    ok

---
+++ Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace ClienteWeb.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+        }
+
+       
+
+        public int Id { get; set; }
+        public string Nome {get; set; }
+        public string Cpf { get; set; }
+        public string Municipio { get; set; }
+        public string Rua { get; set; }
+        public string Cep  { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Estado { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
